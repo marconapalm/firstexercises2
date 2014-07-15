01c90c415fc0bc79bd7526f9684212a20bbb6823
orphan27 <marconapalm@gmail.com>
2014-07-15 20:14:12 +0200

    Game's main class

---
+++ tic-tac-toe-game/tris.java
@@ -0,0 +1,16 @@
+/*
+ * Little tic-tac-toe game. User vs Computer.
+ */
+
+package tris;
+
+/**
+ *
+ * @author Marco_Rapaccini
+ */
+public class Tris {
+    public static void main(String[] args) {
+        Window gui = new Window();  // new istance of game window
+        gui.go();       
+    }
+}   
